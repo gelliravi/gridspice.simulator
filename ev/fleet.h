fa770ed1b9713486fe656744a691b02a38260c8a
Rishi Gharpuray <rgharpuray@gmail.com>
2012-01-11 12:35:19 -0800

    ev starter code

---
+++ ev/fleet.h
@@ -0,0 +1,41 @@
+/** $Id: fleet.h 683 2008-06-18 20:16:29Z d3g637 $
+	@file fleet.h
+	@addtogroup fleet
+	@ingroup MODULENAME
+
+ @{
+ **/
+
+#ifndef _fleet_H
+#define _fleet_H
+
+#include <stdarg.h>
+#include "gridlabd.h"
+
+class fleet {
+private:
+	/* TODO: put private variables here */
+protected:
+	/* TODO: put unpublished but inherited variables */
+public:
+	/* TODO: put published variables here */
+public:
+	/* required implementations */
+	fleet(MODULE *module);
+	int create(void);
+	int init(OBJECT *parent);
+	TIMESTAMP presync(TIMESTAMP t0, TIMESTAMP t1);
+	TIMESTAMP sync(TIMESTAMP t0, TIMESTAMP t1);
+	TIMESTAMP postsync(TIMESTAMP t0, TIMESTAMP t1);
+public:
+	static CLASS *oclass;
+	static fleet *defaults;
+#ifdef OPTIONAL
+	static CLASS *pclass; /**< defines the parent class */
+	TIMESTAMP plc(TIMESTAMP t0, TIMESTAMP t1); /**< defines the default PLC code */
+#endif
+};
+
+#endif
+
+/**@}*/
